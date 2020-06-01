cf5559d01c39b05b32b5647eb8f3cb7b1ad67526
Sam Thomas <59207827+samcthomas912@users.noreply.github.com>
2020-06-01 00:55:31 -0400

    Blocks
    Added spin animation to boxes when hovering (currently not responsive)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
 function fade() {
-  $('.loading').fadeOut("slow");
+  $(".loading").fadeOut("slow");
   }
   setTimeout(fade, 3000);
+
+
+  function resetSpin() {
+    $("#block1").removeClass("spins");
+    $("#block2").removeClass("spins");
+    $("#block3").removeClass("spins");
+    $("#block4").removeClass("spins");
+    $("#block5").removeClass("spins");
+    $("#block6").removeClass("spins");
+    $("#block7").removeClass("spins");
+    $("#block8").removeClass("spins");
+    $("#block9").removeClass("spins");
+    }
+
+  $(".right img").on("click",function(){
+      $("#block1").removeClass("spins").addClass("spins");
+      $("#block2").removeClass("spins").addClass("spins");
+      $("#block3").removeClass("spins").addClass("spins");
+      $("#block4").removeClass("spins").addClass("spins");
+      $("#block5").removeClass("spins").addClass("spins");
+      $("#block6").removeClass("spins").addClass("spins");
+      $("#block7").removeClass("spins").addClass("spins");
+      $("#block8").removeClass("spins").addClass("spins");
+      $("#block9").removeClass("spins").addClass("spins");
+      setTimeout(resetSpin,2000);
+    }
+)
+
+
+  resetSpin();
